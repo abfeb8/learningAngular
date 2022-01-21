9541133abfa0edb95352d5e3f95024a8d6237ebb
Abhishek Malviya <abfeb8@gmail.com>
2022-01-21 19:46:15 +0530

    added item component to store each task item

---
+++ demoProject/src/app/components/item/item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from 'src/app/interfaces/item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+  @Input() item!: Item;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  delItem(title: string) {
+    console.log("Delete" + title);
+
+  }
+}
